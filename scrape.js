2d377f4b804e6938307112352c48174ca4d86ba2
Dan Sullivan <dansullivan@gmail.com>
2016-12-09 08:45:11 -0600

    working on processing of children

--- scrape.js
+++ scrape.js
@@ -9,7 +9,18 @@ request('https://dansullivan.io/wiki/doku.php?id=command_reference',
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
       $('h2').each(function(i, element){
-        console.log($(this).text());
+        var sectiontitle = $(this).text();
+        var nxt = $(this).next();
+        nxt.children().each(function(i, element){
+          //console.log("BEGIN i" + i + " " + $(this).text() + " END i " + i);
+          if ( $(this).is( "p") ) {
+            console.log("it is a p tag");
+          };
+        });
+        //console.log(sectiontitle); 
+        //console.log("  child begin");
+        //console.log("  " + nxt.text());
+        //console.log("  child end");
       });
     }
 });
